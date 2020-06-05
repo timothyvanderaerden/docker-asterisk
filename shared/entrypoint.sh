6005fcc174ce787805f9c6d7d1070979e184f537
Timothy <timothy.vanderaerden@sippy.cloud>
2020-06-05 14:33:04 +0200

    Add monitor stack
    Monitor stack:
      - Prometheus
      - Node exporter
      - cAdvisor
      - Heplify-server (PostgreSQL database)
      - Grafana

--- shared/entrypoint.sh
+++ shared/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Set the IP of the Heplify-server for hep.conf
+HEPLIFY_IP=`getent hosts heplify-server | awk '{ print $1 }'`
+echo capture_address=$HEPLIFY_IP:9060 > /usr/local/etc/asterisk/hep.conf
+chown -R asterisk:asterisk /usr/local/etc/asterisk
+
 if [ "$1" = "" ]; then
   COMMAND="/usr/sbin/asterisk -f -p"
 else
