42b4a40d49b5bb008958c97f4da882a078cde87f
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2020-06-10 14:02:46 +0200

    Enable more codecs
    - G722
    - GSM
    - iLBC
    - Opus
    - Speex

--- shared/menuselect.sh
+++ shared/menuselect.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# codec_resample: allows re-sampling of Signed Linear into different sampling rates 12,16,24,32,44,48,96 or 192 kHz to aid translation.
+# func_channel: required by res_hep to sen statistics
+# pbx_config: used for dialplan config
+
 set -e
 
 cd /tmp/asterisk/
@@ -9,9 +14,19 @@ make menuselect.makeopts \
     --enable app_senddtmf \
     --enable chan_pjsip \
     --enable codec_alaw \
+    --enable codec_g722 \
+    --enable codec_gsm \
+    --enable codec_ilbc \
+    --enable codec_opus \
+    --enable codec_resample \
+    --enable codec_speex \
+    --enable format_gsm \
+    --enable format_ilbc \
     --enable format_pcm \
     --enable func_channel \
     --enable MOH-OPSOUND-ALAW \
+    --enable MOH-OPSOUND-G722 \
+    --enable MOH-OPSOUND-GSM \
     --enable pbx_config \
     --enable res_hep \
     --enable res_hep_pjsip \
