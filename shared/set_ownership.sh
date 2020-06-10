e13ab0852f2830097f0267d173ea5d5ed7150f1e
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2020-06-10 16:46:10 +0200

    Cleanup & reduce image size

--- shared/set_ownership.sh
+++ shared/set_ownership.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+groupadd asterisk
+useradd -r -d /var/lib/asterisk -g asterisk asterisk
 chown -R asterisk:asterisk /var/lib/asterisk \
     && chown -R asterisk:asterisk /var/spool/asterisk \
     && chown -R asterisk:asterisk /var/log/asterisk \
