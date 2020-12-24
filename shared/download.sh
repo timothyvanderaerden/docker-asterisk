530258d369ec36c055d149412d85f44f4933bde3
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2020-12-24 16:41:06 +0100

    Upgrade to Asterisk 18 LTS
    #1

--- shared/download.sh
+++ shared/download.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set version
-ASTERISK_VERSION=16.13.0
+ASTERISK_VERSION=18.1.1
 
 cd /tmp
 wget https://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz \
